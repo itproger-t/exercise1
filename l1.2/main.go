805bf106fbbd7051f93873e9260f89aeb92c8050
itproger-t <dayan020030@gmail.com>
2025-08-17 14:40:09 +0300

    Update main.go

--- l1.2/main.go
+++ l1.2/main.go
@@ -2,28 +2,21 @@ package main
 
 import (
 	"fmt"
-	"slices"
-	"time"
+	"sync"
 )
 
-func CompetitiveSquaring(arr []int) {
-	ch := make(chan struct{}, 5)
-	for i := range arr {
-		ch <- struct{}{}
-		go func(j int) {
-			arr[j] *= arr[j]
-			<-ch
-		}(i)
-	}
-	time.Sleep(time.Duration(1) * time.Second)
-}
-
 func main() {
-	arr1 := []int{1, 2, 3, 4, 5}
-	CompetitiveSquaring(arr1)
-	fmt.Println(slices.Equal(arr1, []int{1, 4, 9, 16, 25}), arr1)
+	numbers := []int{2, 4, 6, 8, 10}
+	var wg sync.WaitGroup
 
-	arr2 := []int{6, 7, 8, 9, 10}
-	CompetitiveSquaring(arr2)
-	fmt.Println(slices.Equal(arr2, []int{36, 49, 64, 81, 100}), arr2)
+	for _, num := range numbers {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			square := n * n
+			fmt.Printf("%d^2 = %d\n", n, square)
+		}(num)
+	}
+
+	wg.Wait()
 }
